24b196a852528d391c056feb33e9f89937a5e4d1
Pizza-beep307 <younes.chelda@gmail.com>
2025-02-12 23:38:18 +0100

    random symbols generator function added

---
+++ symbols_generator.py
@@ -0,0 +1,11 @@
+import random
+symbols_list = []
+for element in range (33, 126):
+    if chr(element).isalpha() == False and chr(element).isdigit() == False:
+        symbols_list.append(chr(element))
+
+def random_symbols():
+    symbol = random.randint(0, len(symbols_list) - 1)
+    return symbols_list[symbol]
+
+print(random_symbols())
